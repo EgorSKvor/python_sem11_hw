4f4c68716a794a7c8af7b420259da787d6daa325
Egor <ee3068316@gmail.com>
2023-01-19 21:48:28 +0300

    add homework

---
+++ test.py
@@ -0,0 +1,17 @@
+# from sympy import symbols, diff
+
+# x, y = symbols('x y')
+# der = diff(0.6*x**3 + 5.5*x**2 + 10*x - 5)
+# # t = str(der.as_independent())
+# # lst = list(t)
+# # new_lst = []
+# # for i in range(len(lst)):
+# #     if lst[i] not in '(),':
+# #         new_lst.append(lst[i])
+
+# # proizv = ''.join(new_lst)
+# # print(proizv)
+# print(der)
+
+a = 1.1
+print(2*a**2)
